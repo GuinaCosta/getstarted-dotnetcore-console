f440ed56d058b07e449a3a4305f34c8ed0323974
guina.costa <a.costa13@hotmail.com>
2020-07-19 04:05:16 -0300

    famigerado initial commit rs

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM microsoft/dotnet:3.0.0-preview2-runtime-alpine3.8
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace getstarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine($"\nArch {RuntimeInformation.OSArchitecture}");
+            Console.WriteLine($"\nOS {RuntimeInformation.OSDescription}");
+            Console.WriteLine($"\nProcArch {RuntimeInformation.ProcessArchitecture}");
+
+            Console.WriteLine("\nQual é o seu nome, mano?");
+            var nome = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nOlá, {nome}, {date:d} às {date:t}");
+            Console.WriteLine("\nAperte qualquer tecla para sair");
+            Console.ReadKey();
+        }
+    }
+}
